21054085c687eb1b8f4165c29114fb4a0cf90237
sanojck <cksanoj@gmail.com>
2021-01-30 21:36:42 +0800

    Apply base styles for project

--- src/App.tsx
+++ src/App.tsx
@@ -3,23 +3,26 @@ import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Login from './app/login/Login';
 import Dashboard from './app/dashboard/Dashboard';
 import PageNotFound from './app/shared/pagenotfound/PageNotFound';
+import './App.scss';
 
 function App() {
   return (
-    <div>
-      <Router>
-        <Switch>
+    <> {/** => `#root` div*/}
+      <div className='txToDo'>
+        <Router>
+          <Switch>
 
-          <Route exact path={`${process.env.PUBLIC_URL}/`} component={Login} />
+            <Route exact path={`${process.env.PUBLIC_URL}/`} component={Login} />
 
-          <Route exact path={`${process.env.PUBLIC_URL}/login`} component={Login} />
-          <Route exact path={`${process.env.PUBLIC_URL}/dashboard`} component={Dashboard} />
+            <Route exact path={`${process.env.PUBLIC_URL}/login`} component={Login} />
+            <Route exact path={`${process.env.PUBLIC_URL}/dashboard`} component={Dashboard} />
 
-          <Route component={PageNotFound} />
+            <Route component={PageNotFound} />
 
-        </Switch>
-      </Router>
-    </div>
+          </Switch>
+        </Router>
+      </div>
+    </>
   );
 }
 
